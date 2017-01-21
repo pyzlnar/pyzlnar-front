34bdb84f0fdab7b91d4a2e786203514fda65c378
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-01-21 11:34:46 -0600

    :sparkles: Added basic React Hello World!
    I don't think I've ever been so happy to see a Hello World. Jesus christ
    why does a SPA needs so much setup?
    
    Oh well, I think I'll eventually get to understand it, little by little.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+// Core Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// Local Imports
+import { App} from './components/App';
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
