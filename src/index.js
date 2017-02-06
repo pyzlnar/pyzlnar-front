fe43ed32f315b481101bec39a180ec5c9e5e6307
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-02-05 18:12:30 -0600

    :wrench: Initial redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 // Core Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 // Local Imports
 import { App } from './components/App';
 import './../styles/app.scss';
 
+// Reducers initialization
+const rootReducer = (state, action) => {
+  return state;
+}
+
+// Store creation
+const store = createStore(rootReducer);
+
 // Root render
 const rootRender = () => {
   return (
+    <Provider store={store}>
     <App/>
+    </Provider>
   );
 }
 ReactDOM.render(rootRender(), document.getElementById('app'));
