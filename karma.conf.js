b0b97418b923562f7f601f80af37d8b68c4298dd
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-02-10 20:55:38 -0600

    :zap: Added tests for navbar
    Uff... man.
    Im definitely not used to having to import things at all. Also I miss
    open class. Makes stubbing so easy in ruby, whilst here you have to do
    some weird sorcery.
    
    I'll have to figure something out for when I actually need stubs I'm
    definitely not getting an api result when running tests. Oh well.

---
+++ karma.conf.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+
+const webpackConfig = {
+  colors: true,
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|jspm_packages)/,
+        loader: 'babel',
+      }
+    ],
+    noParse: [
+      /node_modules\/sinon\//,
+    ],
+  },
+  resolve: {
+    alias: {
+      'sinon': 'sinon/pkg/sinon'
+    }
+  },
+  node: {
+    fs: 'empty'
+  }
+}
+
+module.exports = (config) => {
+  config.set({
+    basePath: './',
+    singleRun: true,
+    frameworks: ['mocha', 'chai-sinon'],
+    keepAlive: true,
+    reporters: ['mocha'],
+    colors: true,
+    preprocessors: {
+      'src/**/*.js':  ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
+    },
+    proxies: {
+      '/static/img/': './img/',
+      '/src/': './src/',
+      '/test/': './test/',
+      '/node_modules/': './node_modules/'
+    },
+    browsers: ['PhantomJS'],
+    files: [
+      'test/**/*.spec.js'
+    ],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true
+    },
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+    client: {
+      captureConsole: true,
+      mocha: {
+        bail: true
+      }
+    }
+  })
+}
