18b5a9dcb2bc191b4527fe3a2c1e12c986df97f3
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-02-12 11:36:44 -0600

    :zap: Added better tests for ApiHelper

--- test/api/ApiHelper.spec.js
+++ test/api/ApiHelper.spec.js
@@ -7,10 +7,12 @@ describe('ApiHelper', () => {
     let spy, handlers
 
     beforeEach(() => {
-      handlers = {
-        success: sinon.spy(),
-        error:   sinon.spy()
-      }
+      const success = sinon.stub()
+      const error   = sinon.stub()
+      success.returns('Success!')
+      error.returns('Error!')
+
+      handlers = { success: success, error: error }
     })
 
     afterEach(() => {
@@ -21,8 +23,9 @@ describe('ApiHelper', () => {
       const json = { was: 'called with a string' }
       fetchMock.get('/string', json)
 
-      request('/string', handlers).then((action) => {
+      request('/string', handlers).then(result => {
         expect(handlers.success).calledWith(json)
+        expect(result).to.equal('Success!')
         done()
       })
     })
@@ -31,8 +34,9 @@ describe('ApiHelper', () => {
       const json = { was: 'called with an object' }
       fetchMock.get('/obj', json)
 
-      request({path: '/obj', opts: {}}, handlers).then(() => {
+      request({path: '/obj', opts: {}}, handlers).then(result => {
         expect(handlers.success).calledWith(json)
+        expect(result).to.equal('Success!')
         done()
       })
     })
@@ -40,17 +44,19 @@ describe('ApiHelper', () => {
     it('handles incorrectly parsed json', done => {
       fetchMock.get('/badjson', 'Not a json')
 
-      request('/badjson', handlers).then(() => {
+      request('/badjson', handlers).then(result => {
         expect(handlers.error).called
+        expect(result).to.equal('Error!')
         done()
       })
     })
 
     it('handles forbiddens and other fulfilled promise status', done => {
-      fetchMock.get('/forbidden', { status: 404 })
+      fetchMock.get('/forbidden', { status: 403 })
 
-      request('/forbidden', handlers).then(() => {
+      request('/forbidden', handlers).then(result => {
         expect(handlers.error).called
+        expect(result).to.equal('Error!')
         done()
       })
     })
@@ -58,8 +64,9 @@ describe('ApiHelper', () => {
     it('handles disconnects and other rejected promise status', done => {
       fetchMock.get('/disconnect', { status: 504 })
 
-      request('/disconnect', handlers).then(() => {
+      request('/disconnect', handlers).then(result => {
         expect(handlers.error).called
+        expect(result).to.equal('Error!')
         done()
       })
     })
