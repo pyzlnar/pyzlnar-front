a9418cb2d40cae5fe9eb78d981d9fd2b69370c98
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-02-18 19:46:57 -0600

    :sparkles: Added basic about sites component
    In all honesty, I don't think this will be much more different later
    down the line. I do have to add links to several of the sites I
    mentioned there, but I'm thinking of having a component to open inside
    links without reloading the app.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { Route, IndexRoute } from 'react-router'
 // Pages
 import { App }            from './components/App';
 import { Home }           from './components/Home';
+import { About }          from './components/About';
 import { SitesContainer } from './components/Sites';
 import { NotFound }       from './components/NotFound';
 
@@ -13,6 +14,7 @@ export const appRoutes = () => {
     <Route component={App} path='/'>
       <IndexRoute component={Home} />
       <Route component={Home}           path='/home'          />
+      <Route component={About}          path='/about(/:who)'  />
       <Route component={SitesContainer} path='/sites(/:site)' />
       <Route component={NotFound}       path='*'              />
     </Route>
